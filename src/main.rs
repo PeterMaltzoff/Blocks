eea94cebd6d34f5d9dee74a63599d1cc2c02efb9
PeterMaltzoff <peter.maltzoff@gmail.com>
2023-01-17 22:51:55 +0000

    Basic block scene

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,54 @@
-use beryllium::*;
+//! A simple 3D scene with light shining over a cube sitting on a plane.
+
+use bevy::prelude::*;
 
 fn main() {
-    let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BlockScene)
+        .run();
+}
 
-    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
-    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
-    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core)
-        .unwrap();
-    #[cfg(target_os = "macos")]
-    {
-        sdl.gl_set_attribute(SdlGlAttr::Flags, ContextFlag::ForwardCompatible)
-            .unwrap();
+pub struct BlockScene;
+
+impl Plugin for BlockScene {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup);
     }
+}
 
-    let _win = sdl
-        .create_gl_window(
-            "Hello Window",
-            WindowPosition::Undefined,
-            800,
-            600,
-            WindowFlags::Shown,
-        )
-        .expect("couldn't make a window and context");
-
-    'main_loop: loop {
-        // handle events this frame
-        while let Some(event) = sld.poll_events().and_then(Result::ok) {
-            match event {
-                Event::Quit(_) => break 'main_loop,
-                _ => (),
-            }
-        }
-        // now the events are clear
-
-        // here's where we could change the world state and draw
-    }
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
